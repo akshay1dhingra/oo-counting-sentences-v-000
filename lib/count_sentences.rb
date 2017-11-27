cac5ecef28fa77826a635466cf90b89ebc2d4df9
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-11-27 23:43:07 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
 
-    self.split(/[.?!]/).delete_if {|x| x.empty?}.count
-
+    self.split(/[.?!]/).delete_if {|x| x.empty?}.count #there is an empty string in the array and in order to delete/reject it you must iterate through the array, check if that string in the array has no characters, then delete.if or you can use .reject as well. 
+    
   end
 end
